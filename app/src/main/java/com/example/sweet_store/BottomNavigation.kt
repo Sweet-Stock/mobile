5fc43a2a15c49cb80fbf0200722548e4a211200c
giovanna.valentin <giovanna.valentin@valemobi.com.br>
2022-10-17 10:35:03 -0300

    Validation and bottom navigation

---
+++ app/src/main/java/com/example/sweet_store/BottomNavigation.kt
@@ -0,0 +1,11 @@
+package com.example.sweet_store
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BottomNavigation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bottom_navigation)
+    }
+}
