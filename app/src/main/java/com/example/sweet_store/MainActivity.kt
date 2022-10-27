cd69e1dc412fd1d47da7c7f91df13f906dfb7a96
abnerlucss <abner.santos@bandtec.com.br>
2022-10-26 22:04:45 -0300

    feat: recycler view na tela de pedidos

--- app/src/main/java/com/example/sweet_store/MainActivity.kt
+++ app/src/main/java/com/example/sweet_store/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.sweet_store.confectionery_profile.ConfectioneryProfile
+import com.example.sweet_store.orders.Orders
 import com.example.sweet_store.page_controll.rating.RatingPage
 import com.example.sweet_store.profile.Profile
 
@@ -11,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        startActivity(Intent(this,  Profile::class.java))
+        startActivity(Intent(this, Orders::class.java))
     }
 
 }
---
+++ app/src/main/java/com/example/sweet_store/orders/OrderHolder.kt
@@ -0,0 +1,10 @@
+package com.example.sweet_store.orders
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class OrderHolder(cardLayout: View) : RecyclerView.ViewHolder(cardLayout) {
+    fun linkLayoutItems(){
+
+    }
+}
