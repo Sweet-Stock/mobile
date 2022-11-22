891fc70ebd9a81d2c3160a806462396c9ab19c1f
abnerlucss <abner.santos@bandtec.com.br>
2022-11-21 22:09:06 -0300

    feat: telas carrinho de compras

---
+++ app/src/main/java/com/example/sweet_store/cart/CheckoutCart.kt
@@ -0,0 +1,11 @@
+package com.example.sweet_store.cart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheckoutCart : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checkout_cart)
+    }
+}
