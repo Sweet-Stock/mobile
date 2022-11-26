8a35f3aade7a18b735c17858cb9c607557cb23a0
abnerlucss <abner.santos@bandtec.com.br>
2022-11-25 22:00:17 -0300

    corrigindo carrinho

--- app/src/main/java/com/example/sweet_store/cart/CheckoutCart.kt
+++ app/src/main/java/com/example/sweet_store/cart/CheckoutCart.kt
@@ -2,6 +2,7 @@ package com.example.sweet_store.cart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sweet_store.R
 
 class CheckoutCart : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
