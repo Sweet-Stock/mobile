fcc3e26e93fa82861213c2c6898731434309cb9f
abnerlucss <abner.santos@bandtec.com.br>
2022-12-02 00:19:53 -0300

    feat: exibindo bandeira do cartão correntamente na listagem de formas de pagamento

--- app/src/main/java/com/example/sweet_store/payments/PaymentMethodHolder.kt
+++ app/src/main/java/com/example/sweet_store/payments/PaymentMethodHolder.kt
@@ -1,14 +1,24 @@
 package com.example.sweet_store.payments
 
+import android.os.Build
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sweet_store.R
 import com.example.sweet_store.model.payment_method.PaymentResponse
 
 class PaymentMethodHolder(cardLayout: View) : RecyclerView.ViewHolder(cardLayout) {
+    @RequiresApi(Build.VERSION_CODES.N)
     fun linkLayoutItems(currentItem: PaymentResponse) {
+        var brands = mapOf<String, Int>(
+            "Elo" to R.drawable.flag_elo,
+            "American Express" to R.drawable.flag_american_express,
+            "Visa" to R.drawable.flag_visa,
+            "MasterCard" to R.drawable.flag_mastercard
+        )
+
         val paymentName: TextView = itemView.findViewById(R.id.payment_name)
         val cardNumber: TextView = itemView.findViewById(R.id.card_number)
         val brandPayment: ImageView = itemView.findViewById(R.id.brand_payment)
@@ -16,7 +26,7 @@ class PaymentMethodHolder(cardLayout: View) : RecyclerView.ViewHolder(cardLayout
 
         paymentName.text = currentItem.paymentName
         cardNumber.text = currentItem.cardNumber
-        brandPayment.setImageResource(R.drawable.flag_mastercard)
+        brandPayment.setImageResource(brands.getOrDefault(currentItem.brand, R.drawable.flag_mastercard))
         expirationDate.text = currentItem.expirationDate.toString()
     }
 }
